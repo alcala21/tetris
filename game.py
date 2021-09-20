bd98493b5fb638e8f346c4af8fb4acf417de6c41
Carlos Alcala <alcala21@gmail.com>
2021-09-20 17:02:22 -0500

    Tetris. Stage 2/4.

--- game.py
+++ game.py
@@ -3,43 +3,100 @@ import numpy as np
 
 
 class Tetris:
-    def __init__(self):
-        self.letters = {}
-        self.grid = self.make_grid()
-        self.get_letters()
+    def __init__(self, rows=20, cols=10):
+        self.letter_dict = {}
+        self.rows = rows
+        self.cols = cols
+        self.grid = self.make_grid(rows, cols)
+        self.make_letter_dict()
+        self.letter = None
+        self.rotation = None
+        self.letter_vals = None
+        self.base = 10
 
-    def get_letters(self):
-        self.letters["I"] = [[1, 5, 9, 13], [4, 5, 6, 7], [1, 5, 9, 13], [4, 5, 6, 7]]
-        self.letters["J"] = [[2, 6, 9, 10], [4, 5, 6, 10], [1, 2, 5, 9], [0, 4, 5, 6]]
-        self.letters["L"] = [[1, 5, 9, 10], [6, 8, 9, 10], [5, 6, 10, 14], [4, 5, 6, 8]]
-        self.letters["O"] = [[5, 6, 9, 10], [5, 6, 9, 10], [5, 6, 9, 10], [5, 6, 9, 10]]
-        self.letters["S"] = [[6, 5, 9, 8], [5, 9, 10, 14], [6, 5, 9, 8], [5, 9, 10, 14]]
-        self.letters["T"] = [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]]
-        self.letters["Z"] = [[4, 5, 9, 10], [2, 5, 6, 9], [4, 5, 9, 10], [2, 5, 6, 9]]
+    def make_letter_dict(self):
+        self.letter_dict["I"] = [[4, 14, 24, 34], [3, 4, 5, 6]]
+        self.letter_dict["J"] = [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]]
+        self.letter_dict["L"] = [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]]
+        self.letter_dict["O"] = [[4, 14, 15, 5]]
+        self.letter_dict["S"] = [[5, 4, 14, 13], [4, 14, 15, 25]]
+        self.letter_dict["T"] = [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]]
+        self.letter_dict["Z"] = [[4, 5, 15, 16], [5, 15, 14, 24]]
 
-    def start(self):
-        letter = input()
-        if letter in self.letters:
-            self.display(self.grid)
+    def start(self, letter):
+        if letter in self.letter_dict:
+            self.letter = letter
+            self.rotation = 0
+            self.adjust_letter()
+            self.base = self.cols
+            self.display_grid()
+            self.display_letter()
 
-            letter_array = self.letters[letter]
-            self.display(self.add_letter(self.grid.copy(), letter_array[0]))
+            while True:
+                command = input()
+                if command == "rotate":
+                    self.rotate()
+                if command == "right":
+                    self.right()
+                if command == "left":
+                    self.left()
+                if command == "down":
+                    self.down()
+                if command == "exit":
+                    break
+                self.display_letter()
 
-            for i in range(1, 5):
-                letter_points = letter_array[i % 4]
-                self.display(self.add_letter(self.grid.copy(), letter_points))
 
-    def make_grid(self):
-        return np.array([["-"]*4] * 4)
+    def make_grid(self, rows, cols):
+        return np.array([["-"] * cols] * rows)
 
     def display(self, grid):
         print("\n".join(" ".join(x) for x in grid), end="\n\n")
 
+    def display_grid(self):
+        self.display(self.grid)
+
+    def display_letter(self):
+        self.display(self.add_letter(self.grid.copy(), self.letter_vals[self.rotation]))
+
     def add_letter(self, grid, arr):
         for point in arr:
-            grid[point // 4, point % 4] = "0"
+            grid[(point // self.cols) % self.rows, point % self.cols] = "0"
         return grid
 
+    def adjust_letter(self):
+        self.letter_vals = [[((x // self.base) * self.cols) + ((x % self.base) % self.cols) for x in pos]
+                            for pos in self.letter_dict[self.letter]]
 
-Tetris().start()
+    def rotate(self):
+        self.down()
+        self.rotation += 1
+        self.rotation = self.rotation % len(self.letter_vals)
+
+    def right(self):
+        self.down()
+        self.letter_vals = [[((x // self.base) * self.cols) + (((x + 1) % self.base) % self.cols) for x in pos]
+                            for pos in self.letter_vals]
+
+    def left(self):
+        self.down()
+        self.letter_vals = [[((x // self.base) * self.cols) + (((x - 1 + self.base) % self.base) % self.cols) for x in pos]
+                            for pos in self.letter_vals]
+
+    def down(self):
+        self.letter_vals = [[(((x + self.base) // self.base) * self.cols) + ((x % self.base) % self.cols) for x in pos]
+                            for pos in self.letter_vals]
+
+
+def start_game():
+    letter = input()
+    dimensions = input()
+    ncols, nrows = (int(x) for x in dimensions.split())
+    myTetris = Tetris(nrows, ncols)
+    myTetris.start(letter)
+
+start_game()
+
+
+
 
